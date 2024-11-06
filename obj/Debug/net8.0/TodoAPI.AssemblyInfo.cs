8760309b7aa04165b0e18570bdc378111e21b929
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-11-06 07:45:07 -0500

    fix issues

--- obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b647edc7f67c6c4d3aa1da7f82f7bed37831ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
